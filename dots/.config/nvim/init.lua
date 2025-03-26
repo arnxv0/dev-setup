7174c019d42665a090c11d4f6c9f404ae141f813
Arnav Dewan <57629464+arnxv0@users.noreply.github.com>
2025-03-27 00:16:32 +0530

    Configure tailwind

--- dots/.config/nvim/init.lua
+++ dots/.config/nvim/init.lua
@@ -810,6 +810,8 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
+        astro = {},
+        tailwindcss = {},
         ts_ls = {},
         lua_ls = {
           -- cmd = { ... },
