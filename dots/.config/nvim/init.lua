d8cb1f53fa8e865fee6e0f91b262bbbfdd125799
Arnav Dewan <57629464+arnxv0@users.noreply.github.com>
2025-04-11 21:14:43 +0530

    Add new colour scheme

--- dots/.config/nvim/init.lua
+++ dots/.config/nvim/init.lua
@@ -1073,6 +1073,39 @@ require('lazy').setup({
   --     vim.cmd.colorscheme 'vscode'
   --   end,
   -- },
+  -- {
+  --   'rebelot/kanagawa.nvim',
+  --   priority = 1000, -- Make sure to load this before all the other start plugins.
+  --   opts = {
+  --     compile = false, -- enable compiling the colorscheme
+  --     undercurl = true, -- enable undercurls
+  --     commentStyle = { italic = true },
+  --     functionStyle = {},
+  --     keywordStyle = { italic = true },
+  --     statementStyle = { bold = true },
+  --     typeStyle = {},
+  --     transparent = false, -- do not set background color
+  --     dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+  --     terminalColors = true, -- define vim.g.terminal_color_{0,17}
+  --     colors = { -- add/modify theme and palette colors
+  --       palette = {},
+  --       theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+  --     },
+  --     overrides = function(colors) -- add/modify highlights
+  --       return {}
+  --     end,
+  --     theme = 'wave', -- Load "wave" theme
+  --     background = { -- map the value of 'background' option to a theme
+  --       dark = 'wave', -- try "dragon" !
+  --       light = 'lotus',
+  --     },
+  --   },
+  --   config = function()
+  --     vim.opt.background = 'dark' -- set this to dark or light
+  --     vim.cmd.colorscheme 'kanagawa'
+  --   end,
+  -- },
+
   -- Highlight todo, notes, etc in comments
   {
     'folke/todo-comments.nvim',
